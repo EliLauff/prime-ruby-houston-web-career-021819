52061e11fcc6602f12ec7b79323b4f781780933d
Eli Lauffenburger <eli.lauffenburger@gmail.com>
2019-02-13 18:18:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,4 @@
 # Add  code here!
 def prime?(int)
-  counter = 2
   
-  if int == 2
-    return true
-  end
-  
-  while counter < int
-    if int%counter == 0
-      return false
-    else
-      counter += 1
-    end
-  end
-  return true
 end
